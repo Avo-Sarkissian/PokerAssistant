dac00013c8a411fb9dd358849bfd1d13401c14c9
Gonmyh-7konwe-funmuw <newborn.jujitsu0w@icloud.com>
2025-07-27 01:27:03 +0400

    Initial Commit

---
+++ PokerAssistant/PokerAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerAssistantApp.swift
+//  PokerAssistant
+//
+//  Created by Avo Sarkissian on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PokerAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokerAssistantTests/PokerAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokerAssistantTests.swift
+//  PokerAssistantTests
+//
+//  Created by Avo Sarkissian on 7/27/25.
+//
+
+import Testing
+@testable import PokerAssistant
+
+struct PokerAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
